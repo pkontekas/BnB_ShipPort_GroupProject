bcc7275463ad322ddeb0f3360c2be2f9d478b3cd
Philip Kontekas <61799881+pkontekas@users.noreply.github.com>
2020-08-01 14:54:10 +0300

    initial commit
    spring boot initializer project for the bnb group project

---
+++ src/main/java/spring/bnb/boats/BnbShipPortGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package spring.bnb.boats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnbShipPortGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnbShipPortGroupProjectApplication.class, args);
+	}
+
+}
