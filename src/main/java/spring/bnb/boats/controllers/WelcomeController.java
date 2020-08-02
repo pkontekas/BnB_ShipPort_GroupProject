260bf5853ad496a19cf5546e1d459c3d89900496
pkontekas <pkontekas@192.168.1.8>
2020-08-02 20:41:04 +0300

    fixed controller package name

--- src/main/java/spring/bnb/boats/controllers/WelcomeController.java
+++ src/main/java/spring/bnb/boats/controllers/WelcomeController.java
@@ -1,4 +1,4 @@
-package spring.bnb.controllers;
+package spring.bnb.boats.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
